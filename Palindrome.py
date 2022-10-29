97c1114b1602fc1f937b38809bbd7aab2af69819
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2022-10-29 09:17:59 +0530

    Create Palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:return False
+        div=1
+        while (x>=10*div):
+            div *=10
+        while x:
+            if(x%10!=x//div):return False
+            x=(x%div)//10
+            div=div/100
+        return True
+        
+        
+        */Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left./*
