1eb57df8373b92a22a77ce9c53e18e693fc1251a
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2022-11-01 17:53:26 +0530

    Update Longest common prefix.py

--- Longest common prefix.py
+++ Longest common prefix.py
@@ -10,4 +10,7 @@ class Solution:
     ----------------------------------------------------
     Input: strs = ["flower","flow","flight"]
 Output: "fl"
-      
+    ---------------------------------------------------
+    Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
