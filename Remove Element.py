8e3a076362199f8bd3c95ed2fa0c93dbe50ad260
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2022-11-02 16:54:12 +0530

    Update Remove Element.py

--- Remove Element.py
+++ Remove Element.py
@@ -7,5 +7,5 @@ class Solution:
                 k+=1
         return k        
 
-    Input: nums = [3,2,2,3], val = 3
-Output: 2, nums = [2,2,_,_]
+   # Input: nums = [3,2,2,3], val = 3
+#Output: 2, nums = [2,2,_,_]
