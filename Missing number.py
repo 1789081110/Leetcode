2d8d798ba8b896b44ce3abff0b906a37fa5258e9
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2022-11-06 09:03:17 +0530

    Update Missing number.py

--- Missing number.py
+++ Missing number.py
@@ -1,3 +1,4 @@
+#Approach 1
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums) + 1
@@ -5,3 +6,18 @@ class Solution:
             if num not in nums:
                 return num
               
+   ------------------------   -------------------------
+#Approach 2
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        i=0
+        for j in nums:
+            if i==j:
+                i+=1
+        return i        
+----------------------------   -------------------------
+#output
+#Input: nums = [3,0,1]
+#Output: 2
+#Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
