18a1617c4ea7780d88ffb1a980125f9f7f736929
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2022-11-06 09:47:21 +0530

    Update Running sum of 1D array.py

--- Running sum of 1D array.py
+++ Running sum of 1D array.py
@@ -5,6 +5,6 @@ class Solution:
         return nums    
 
     --------------  --------------------
-    Input: nums = [1,2,3,4]
-Output: [1,3,6,10]
-Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+ #Input: nums = [1,2,3,4]
+#Output: [1,3,6,10]
+#Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
