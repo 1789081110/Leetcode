cc2603a95a23a5fef2d09472da711e23cc6dd4e1
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2022-11-07 18:56:13 +0530

    Update Find piviot index.py

--- Find piviot index.py
+++ Find piviot index.py
@@ -10,3 +10,10 @@ class Solution:
                 left+=nums[i]
         return -1       
         
+------------- ------------------
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
