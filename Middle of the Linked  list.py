16664829e66ce5cf0d98b31df408b287681c68fd
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2022-11-09 18:10:34 +0530

    Update Middle of the Linked  list.py

--- Middle of the Linked  list.py
+++ Middle of the Linked  list.py
@@ -8,3 +8,7 @@ class Solution:
             length+=1
             curr=curr.next
         return ListNodes[length//2]
+----------  --------------------
+#Input: head = [1,2,3,4,5]
+#Output: [3,4,5]
+#Explanation: The middle node of the list is node 3.
