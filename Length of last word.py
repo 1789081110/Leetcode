817665d5337daf868590d25807b4d279986ac144
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2022-11-13 17:47:35 +0530

    Update Length of last word.py

--- Length of last word.py
+++ Length of last word.py
@@ -2,3 +2,7 @@ class Solution:
     def lengthOfLastWord(self, s):
         return len(s.strip().split(' ')[-1])
         
+--------------      -----------------
+#Input: s = "Hello World"
+#Output: 5
+#Explanation: The last word is "World" with length 5.
