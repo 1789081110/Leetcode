fd2a612f204a3b840974ed3924475dffe93b97fc
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2022-11-14 18:14:27 +0530

    Update Plus one.py

--- Plus one.py
+++ Plus one.py
@@ -3,3 +3,8 @@ class Solution:
          return map(int,str(int("".join(map(str,digits)))+1))
       
 --------------        --------------------------
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
