09f518c14589d6c304f55e90c46d10155d991c52
Vishnu Kumar <80009462+1789081110@users.noreply.github.com>
2022-11-17 20:08:53 +0530

    Update Count Negative.py

--- Count Negative.py
+++ Count Negative.py
@@ -7,6 +7,6 @@ class Solution:
                     count+=1
         return count
 -----------    -------------
-Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
-Output: 8
-Explanation: There are 8 negatives number in the matrix.
+#Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+#Output: 8
+#Explanation: There are 8 negatives number in the matrix.
